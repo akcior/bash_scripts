9841d526a3c35537bc13b45a8b939b12c7422256
akcior <akcior1@gmail.com>
2019-04-02 23:57:21 +0200

    making loop in script 2 to search files one by one without exiting script.

--- skrypt2/skrypt2.sh
+++ skrypt2/skrypt2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-NAZWA="szukajka.txt"
-KATALOG="/home/linuxmint/data"
-ROZMIAR="1"
-OWNER="linuxmint"
+NAZWA="plik.txt"
+KATALOG="/home/jakub/data"
+ROZMIAR="0"
+OWNER="jakub"
 ZAWARTOSC="tekst"
 while [ "$WYBOR" != "7" ] ; do
     printf "\033c"
@@ -29,25 +29,59 @@ while [ "$WYBOR" != "7" ] ; do
 	"1") read -p"Wpisz nazwe pliku: " NAZWA ;;
 	"2") read -p"Wpisz katalog startowy: " KATALOG ;;
 	"3") read -p"Wpisz minimalny rozmiar " ROZMIAR ;;
-	"4") read -p"Wpisz typ pliku: " TYP ;;
+	"4") read -p"Wpisz wlasciciela pliku: " OWNER ;;
 	"5") read -p"Wpisz zawartosc pliku: " ZAWARTOSC  ;;
 	"6") SZUKAJ="true" ;;
 	"7")  ;;
 	*) echo "Nie ma takiego filtru. Wybierz liczbe od 1 do 7"; WYBOR=0 ;;
     esac
     
+    COMMAND="find"
+    
     if [ "$SZUKAJ" = "true" ]
     then
-	FILE=$(find $KATALOG -name "$NAZWA" -user $OWNER -size $ROZMIAR -exec grep -l "$ZAWARTOSC" {} \; | wc -l)
-	echo $FILE
+
+	 if [ -n "$KATALOG" ]
+	   then
+	       COMMAND="$COMMAND $KATALOG"
+	   else
+	       COMMAND="$COMMAND /"
+	   fi
+	   
+	   if [ -n "NAZWA" ]
+	   then
+	       COMMAND="$COMMAND -name $NAZWA"
+	   fi
+	   
+	   if [ -n "$OWNER" ]
+	   then
+	       COMMAND="$COMMAND -user $OWNER"
+	   fi
+	   
+	   if [ -n "$ROZMIAR" ]
+	   then
+	       COMMAND="$COMMAND -size +$ROZMIAR"
+	   fi
+	   
+	   if [ -n "$ZAWARTOSC" ]
+	   then
+	       COMMAND="$COMMAND -exec grep -l \"$ZAWARTOSC\" {} \\;"
+	   fi
+	    COMMAND="$COMMAND | wc -l"
+
+        FILE=$(eval $COMMAND);
+#	echo $FILE
        if [ $FILE -gt 0 ]
        then
   	   echo "Znaleziono plik"
-	   WYBOR=7
+	   SZUKAJ="false"
+#	   WYBOR=7
        else
 	   echo "Nie znaleziono pliku."
-	   WYBOR=7
+	   SZUKAJ="false"
+#	   WYBOR=7
        fi
+       read C
     fi
 
 done
